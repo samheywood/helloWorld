e5b3e2afc2e3a74b630070d11eac7a94954b5f7e
Sam Heywood <sam.heywood@yahoo.com>
2011-12-26 00:23:34 -0600

    Added migrate scripts for question_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111224055013) do
+ActiveRecord::Schema.define(:version => 20111226061820) do
 
   create_table "answers", :force => true do |t|
     t.string   "answer"
     t.datetime "time"
     t.text     "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "question_id"
+  end
+
+  create_table "items", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
